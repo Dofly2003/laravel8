047ab37b8bea0fba1bd19e46473d82a1ee9bba3d
Dony <doniananda622@gmail.com>
2024-08-12 16:31:45 +0700

    'laravel8'

---
+++ resources/views/home.blade.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="container hidden mx-auto md:block max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <header class="flex flex-col justify-center item-center bg-blue-200">
+            <div>
+                <h1 class="text-4xl flex justify-center">Dinamika  Indo Media</h1>
+                <div class="w-full  justify-center flex">
+                    <p class="w-full max-w-2xl text-center font-thin text-gray-500 text-sm">PT. DINAMIKA INDO MEDIA berdiri pada hari Kamis tanggal 10 Februari 2011 di Kabupaten Bekasi, saat ini kami bergerak dibidang pengadaan barang untuk pemenuhan kebutuhan instansi, Lembaga, dan satuan kerja pemerintahan yang bertempat di Kota Surabaya.</p>
+                </div>
+            </div>
+
+            {{-- <div class="flex ">
+                <img src="https://dinamikaindomedia.co.id/sliders/ZOje5kj4ZCSup0iXRkOf2p4uzMqMaH6T1vV5B2Fc.png" class="w-full max-w-6xl" alt="">
+                <div class="absolute right-0 h-4/6 rounded-tr-xl rounded-bl-xl w-full max-w-md bg-blue-500">
+                    <p class="text-white px-10 pt-10 text-3xl">Anda mencari mitra yang dapat diandalkan dalam mengembangkan bisnis Anda?</p>
+                </div>
+            </div> --}}
+        </header>
+        <body>
+            <div class="flex flex-col gap-8">
+                <div>
+                <h1 class="text-4xl flex justify-center">Unit Bisnis</h1>
+                <div class="w-full  justify-center flex">
+                    <p class="w-full max-w-2xl text-center font-thin text-gray-500 text-sm">Perusahaan kami bergerak di dua unit bisnis:</p>
+                </div>
+                <div class="bg-red-100 flex gap-4 justify-center items-center ">
+                    <div class="w-96  h-44 bg-gray-300">
+                        <div class='top border-b-2 text-white text-2xl py-1 text-center'>
+                            Retail
+                        </div>
+                        <p class="ub px-10 text-gray-500 ">
+                            PT. Dinamika Indo Media adalah penyedia jasa terkemuka di bidang konstruksi dan bangunan yang bertujuan untuk membantu Anda mewujudkan proyek-proyek impian Anda.
+                        </p>
+                    </div>
+                    <div class="w-96 h-44 bg-gray-300">
+                        <div class='top border-b-2 text-white text-2xl py-1 text-center'>Penyedia Jasa</div>
+                        <p class="ub px-10 text-gray-500 ">
+                            PT. Dinamika Indo Media adalah penyedia solusi retail yang berkomitmen untuk memberikan layanan dan teknologi inovatif kepada pelanggan kami. Kami hadir untuk membantu bisnis Anda.
+                        </p>
+                    </div>
+                </div>
+                <h1 class="text-4xl flex justify-center">Support Brand</h1>
+                <div class="w-full  justify-center flex ">
+                    <div class="gap-5 flex-wrap item-center w-96 flex    ">
+                        <img src="https://dinamikaindomedia.co.id/brands/BEwtk9M1UwhfRUztL8JXw8w7dIB0HCrugRRu0P18.png" class="w-20 object-contain" alt="">
+                        <img src="https://dinamikaindomedia.co.id/brands/a8pQ5F8EwUfLsNGzogKFG0ydCwqL3IEAt4fPvE7f.png" class="w-20 object-contain" alt="">
+                        <img src="https://dinamikaindomedia.co.id/brands/bPRxWjWq8gJEW9APClpy0d9P2DEECBReBkTAQrpm.png" class="w-20 object-contain" alt="">
+                        <img src="https://dinamikaindomedia.co.id/brands/yIQOqyP0VwLM9H3UowAfamBoyzWXf0pvNrOw6zUC.png" class="w-20 object-contain" alt="">
+                        <img src="https://dinamikaindomedia.co.id/brands/yIQOqyP0VwLM9H3UowAfamBoyzWXf0pvNrOw6zUC.png" class="w-20 object-contain" alt="">
+                        <img src="https://dinamikaindomedia.co.id/brands/jlcaWNCWbRKKlX28VA0ekyanSdfD94qJfsdz4T6c.png" class="w-20 object-contain" alt="">
+                        <img src="https://dinamikaindomedia.co.id/brands/IWWMeqCXJSijxo7F0E3iF97rNuEpbD9yiNwnxaWe.png" class="w-20 object-contain" alt="">
+                        <img src="https://dinamikaindomedia.co.id/brands/YloE7NnpGYJsxPlmfi7C3qmULtjfRlvyHyGw1z5w.png" class="w-20 object-contain" alt="">
+                        <img src="https://dinamikaindomedia.co.id/brands/nTxKCAaA1yktW1N48ezPKFxJBL29INB3rWzPBKsc.png" class="w-20 object-contain" alt="">
+                        <img src="https://dinamikaindomedia.co.id/brands/O4pBLdAZDKVk3q8NJlhIMyVS5IyQoXCbAxQhQmwQ.png" class="w-20 object-contain" alt="">
+                        <img src="https://dinamikaindomedia.co.id/brands/LRUFTEfJAoaID95dfCBJoi3MAh00G5lPGuH1wFSR.png" class="w-20 object-contain" alt="">
+                        <img src="https://dinamikaindomedia.co.id/brands/3ekrDH6aeJL4eBHGagASlpijy4yMg8ktkFtagDmv.png" class="w-20 object-contain" alt="">
+                        <img src="https://dinamikaindomedia.co.id/brands/9HjYzMS80vzhzFyu4qcwZ4IFXu8O4SgdlsthlMT3.png" class="w-20 object-contain" alt="">
+                        <img src="https://dinamikaindomedia.co.id/brands/X9XfkaPw03pwQtDs1s2EO9FMW5Y8D59TgOd0wX1h.png" class="w-20 object-contain" alt="">
+                        <img src="https://dinamikaindomedia.co.id/brands/6bQMeHD8TiSx5yHNPCQjsEVCsp08NrFLBIMDvNL0.png" class="w-20 object-contain" alt="">
+                        <img src="https://dinamikaindomedia.co.id/brands/Kzj3BZwviLfXrohkkgBnzUmxeG4J91dwhmZZlvYh.png" class="w-20 object-contain" alt="">
+                        <img src="https://dinamikaindomedia.co.id/brands/9HmNVYFLUan8hnHcOeJG34MmCMPvg2mFsiFDuQvk.png" class="w-20 object-contain" alt="">
+                        <img src="https://dinamikaindomedia.co.id/brands/3UUAPsSeCn8AyIAOmF52vDnCzqlWe3nlHjC41huL.png" class="w-20 object-contain" alt="">
+                        <img src="https://dinamikaindomedia.co.id/brands/Q8voEAdP9NPXsDqjp6rqxrFzGUvij5V1HPf3Xpyn.png" class="w-20 object-contain" alt="">
+                    </div>
+                </div>
+            </div>
+
+
+
+
+                <div class="rounded-lg">
+                    <div class="h-64 bg-red-300 rounded-t-lg flex mt-7 pl-7"> 
+                        <div class="pt-7 w-1/3">
+                            <h2>Penyedia Jasa</h2>
+                            <ul class="pl-10 gap-2 flex flex-col list-disc">
+                                    <li>Melayani Pembangunan Sekolah</li=>
+                                    <li>Melayani Pembangunan Kantor/Instansi</li>
+                                    <li>Melayani Pembangunan Gudang</li>
+                                </ul>
+                            </div>
+                            <div class="flex justify-center items-center bg-red-500 rounded-tr-lg w-full">
+                                image
+                            </div>
+                    </div>
+                    <div class="flex justify-end rounded-b-lg bg-blue-300">
+                        <div class="flex justify-center items-center bg-blue-500 rounded-bl-lg w-full">
+                            image
+                        </div>
+                        <div class=" pl-7 pt-7">
+                            <h2>Retail</h2>
+                            <ul class="pl-10 list-disc flex flex-wrap gap-2  h-56 w-96 flex-col ">
+                                <li>Moco</li>
+                                <li>Multimedia</li>
+                                <li>Meubelair</li>
+                                <li>Alat Praktik</li>
+                                <li>APE</li>
+                                <li>Alat Kesehatan</li>
+                                <li>Alat Tulis Kantor</li>
+                                <li>Pupuk Hens</li>
+                                <li>Hibaflor Pupuk</li>
+                                <li>Pupuk Grand Tomiks</li>
+                                <li>Traktor</li>
+                            </ul>
+                    </div>
+
+                    </div>
+                </div>
+
+
+                <div class="">
+                    <div class="text-center my-10">
+                        <h3>Produk  Kami</h3>
+                        <p>Kami telah meningkatkan kinerja perusahaan dengan menjalin kerja sama bersama beberapa klien.</p>
+                    </div>
+                    <div class="flex flex-wrap gap-4">
+
+                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <img class="rounded-t-lg object-contain" src="https://dinamikaindomedia.co.id/katalogs/Pi6sSSyIBPxXY5KzLcgFm5tsVV5y1QlJ8qRKIiHP.jpg" alt="" />
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Read more
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <img class="rounded-t-lg object-contain" src="https://dinamikaindomedia.co.id/katalogs/ThM40g2KHUzAbKPYqPHBg08jZZO1BlmqGwj0KeVK.jpg" alt="" />
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Read more
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <img class="rounded-t-lg object-contain" src="https://dinamikaindomedia.co.id/katalogs/OqEUdk25vMldLA5ACCmhl0ngIs1lDNtO3TIR0GGu.jpg" alt="" />
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Read more
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <img class="rounded-t-lg object-contain" src="https://dinamikaindomedia.co.id/katalogs/RawG5jkzrZvkJeZKVQadAf50bN61wnlOKPH665lx.jpg" alt="" />
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Read more
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <img class="rounded-t-lg object-contain" src="https://dinamikaindomedia.co.id/katalogs/PjogJG0wsXFkR9maXVzfhpSvTtYcRHZzCgStLQAQ.jpg" alt="" />
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Read more
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <img class="rounded-t-lg object-contain" src="https://dinamikaindomedia.co.id/katalogs/6iALTVvVUac6YUwHjHUVF4dAzTPjLsbAuTdh1KOg.jpg" alt="" />
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Read more
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+    </div>
+   
+</body>
+</html>
